908e0a5775ba3cbdf06dd12ee7df85c6c2b27012
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2022-03-12 13:23:07 +0530

    commited

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     GOOGLE_ID:'877750131639-0dkhgv80kc2ch93dpfmvf75mqs6lhau1.apps.googleusercontent.com',
     GOOGLE_SECRET:'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',
     NEXT_AUTH_URL:'localhost:3000/',
+    JWT_SECRET:'GOCSPX-7nJ0ZH3zgn8',
   },
   images:{
     domains:['links.papareact.com','fakestoreapi.com'],
